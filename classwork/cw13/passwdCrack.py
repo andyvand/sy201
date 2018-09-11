345e970411bf930a9c9d2d68682b17d59630a9ff
Pete Nardi <pete@nardi.com>
2018-09-10 21:13:13 -0400

    Repo Updates
    - Tweak to the Python methods reference
    
    - Pre-loaded materials for cw13.

---
+++ classwork/cw13/passwdCrack.py
@@ -0,0 +1,9 @@
+import hashlib
+
+# Here's an example that shows the syntax for calculating an md5 hash:
+# plainText = "Hello World"
+# hashText = hashlib.md5(plainText.encode()).hexdigest()
+
+DICT = "/usr/share/dict/american-english"
+HASHES = "hashes.txt"
+MAXLINES = 10
