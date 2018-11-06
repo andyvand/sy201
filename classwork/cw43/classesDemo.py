b4045aee582b5b60868011dc4abbc91700ae7494
Peter Nardi <nardi@usna.edu>
2018-11-06 11:01:21 -0500

    Repo updates
    - Pre-staged material for cw43 (Classes)

---
+++ classwork/cw43/classesDemo.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+
+# class Midshipman:
+#    alpha = 211234
+#    company = 13
+# 
+# mid1 = Midshipman()
+# mid2 = Midshipman()
+# 
+# print(mid1.alpha,mid1.company)
+# print(mid2.alpha,mid2.company)
+#
+# ---------------------------------------------------------------------
+#
+# class Midshipman:
+#    def __init__(self,alphaCode,companyNumber):
+#       self.alpha = alphaCode
+#       self.company = companyNumberalpha
+# 
+# mid1 = Midshipman(211234,13)
+# mid2 = Midshipman(219999,8)
+# 
+# print(mid1.alpha,mid1.company)
+# print(mid2.alpha,mid2.company)
+#
+# ---------------------------------------------------------------------
+#
+# class Midshipman:
+# 
+# 	def __init__ (self,alpha,company):
+# 		self.alpha = alpha
+# 		self.company = company
+# 
+# 	def regiment(self):
+# 		if self.company <= 15:
+# 			reg = "First"
+# 		else:
+# 			reg = "Second"
+# 		return reg
+# 
+# mid1 = Midshipman(211234,13)
+# print(mid1.regiment())
+# mid2 = Midshipman(219999,22)
+# print(mid2.regiment())
+#
+# ---------------------------------------------------------------------
+#
+# class Midshipman:
+# 
+# 	def __init__ (self,alpha,company,prtScore):
+# 		self.alpha = alpha
+# 		self.company = company
+# 		self.prtScore = prtScore
+# 
+# 	def prtBonus(self,additionalPoints):
+# 		self.prtScore += additionalPoints
+# 
+# mid1 = Midshipman(211234,13,81)
+# print(mid1.prtScore)
+# mid1.prtBonus(7)
+# print(mid1.prtScore)
+#
+# ---------------------------------------------------------------------
+#
+# class Midshipman:
+# 	def __init__ (self,alpha,prtScore):
+# 		self.alpha = alpha
+# 		self.prtScore = prtScore
+# 
+# class Company:
+# 	def __init__ (self):
+# 		self.mids = []
+# 
+# 	def addMid (self,mid):
+# 		self.mids.append(mid)
+# 
+# 	def printScores (self):
+# 		for mid in self.mids:
+# 			print(mid.alpha,mid.prtScore)
+# 
+# # New empty Company
+# co13 = Company()
+# 
+# # Create and add mids
+# mid = Midshipman(211234,81)
+# co13.addMid(mid)
+# mid = Midshipman(218888,93)
+# co13.addMid(mid)
+# mid = Midshipman(219999,88)
+# co13.addMid(mid)
+# 
+# # Print prt scores for all mids
+# co13.printScores()
---
+++ classwork/cw43/cyberStudentTemplate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+# Note, Atom may not be able to correctly color-code the syntax in your class
+# method headers until you start writing the body of the methods.
+
+class CyberStudent:
+
+	# This is your initializer.  Every class should have one.  This one
+	# initializes a new CyberStudent object by creating an alpha property (set to
+	# the alpha parameter in the initializer), and creating a hwScores property
+	# set to an empty list. Returns: Nothing.
+	def __init__(self,alpha):
+
+	# This method appends the score parameter to the hwScores property of a
+	# CyberStudent object. Returns: Nothing
+	def addHW(self,score):
+
+	# This method computes the average of all the scores in the hwScores property
+	# of a CyberStudent object. Returns: hwAverage : float
+	def average(self):
+		
+	
+def main():
+	# Your short program to test your class goes here.	
+	
+main()
